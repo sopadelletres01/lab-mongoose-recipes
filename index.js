eafc16a367598b98b70c32894e36f0485a2b179d
macape01 <macape@fp.insjoaquimmir.cat>
2022-06-23 16:56:49 +0200

    siuu

--- index.js
+++ index.js
@@ -8,16 +8,36 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
+
+(async ()=>{
+  try{
+    const x = await mongoose.connect(MONGODB_URI)
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+    //Limpiar BD
+    await Recipe.deleteMany()
+
+    const recipes = await Recipe.insertMany(data)
+    recipes.forEach(recipe=>console.log(recipe.title))
+    /* const recipesTitles = await Recipe.find({},"title")
+    console.log(recipesTitles) */
+
+    //Usamos el tercer parametro {new:true} para que devuelva el objeto actualizdo
+    const doc = await Recipe.findOneAndUpdate(
+      {name:"Rigatoni alla Genovese"},
+      {duration:100},
+      {new:true}
+    )
+    console.log("Recipe succesfully updated: ",doc)
+
+    const deleted = await Recipe.deleteOne({title: "Carrot Cake"})
+    console.log("Recipe succesfully deleted",deleted)
+
+    //Cerramos conexion (por lo que parece es indiferente usar await aqui)
+    await mongoose.connection.close()
+    console.log("Closing DB...")
+  }
+  catch(error){
     console.error('Error connecting to the database', error);
-  });
+
+  }
+})()
